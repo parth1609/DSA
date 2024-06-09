401a9de0f23a69de921cf7605de85d04d0e39ced
parth1609 <115941606+parth1609@users.noreply.github.com>
2024-06-10 00:56:24 +0530

    Check_if_Array_Is_Sorted_and_Rotated.py

---
+++ Dsa_questions/Check_if_Array_Is_Sorted_and_Rotated.py
@@ -0,0 +1,10 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+
+        count  = 0
+        for i in range(len(nums)):
+            if nums[i] > nums[(i+1)%len(nums)]:
+                count +=1
+            if count > 1:
+                return False
+        return True

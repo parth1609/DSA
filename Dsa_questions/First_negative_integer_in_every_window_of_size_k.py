bc9310fb0e8324b58056cbda864a16bb019b314b
parth1609 <115941606+parth1609@users.noreply.github.com>
2024-06-24 15:10:32 +0530

    First_negative_integer_in_every_window_of_size_k.py

---
+++ Dsa_questions/First_negative_integer_in_every_window_of_size_k.py
@@ -0,0 +1,23 @@
+# First negative integer in every window of size k
+
+
+def printFirstNegativeInteger( A, N, K):
+    # code here
+    i = j = 0
+    li = []
+    ans = []
+    while  j < N:
+        if A[j] < 0:
+            li.append(A[j])
+        if (j-i+1) > K:
+            j+=1
+        elif (j-i+1) == K:
+            if li: # if li not empty 
+                ans.append(li[0])
+                if A[i] == li[0]:
+                    li.pop(0)
+            else:
+                ans.append(0)
+            i+=1
+        j+=1
+    return ans

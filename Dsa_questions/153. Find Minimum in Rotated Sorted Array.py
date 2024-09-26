55bbb39c3d53a979b6684b7931210853de86c300
parth1609 <115941606+parth1609@users.noreply.github.com>
2024-09-26 16:13:15 +0530

    Update 153. Find Minimum in Rotated Sorted Array.py
    just time complexity improves

--- Dsa_questions/153. Find Minimum in Rotated Sorted Array.py
+++ Dsa_questions/153. Find Minimum in Rotated Sorted Array.py
@@ -3,11 +3,11 @@ class Solution:
         length = len(nums)
         left = 0
         right = length-1
-        while left <= right:
+        while left < right:
             mi = (left+right)//2
             if nums[mi] > nums[right]:
-                left += 1
+                left  = mi + 1
             else:
-                right -= 1
+                right = mi
         
-        return nums[mi]
+        return nums[left]

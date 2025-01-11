e9dea097169f136b266505dbec937b72a18d25c0
parth1609 <115941606+parth1609@users.noreply.github.com>
2025-01-11 15:16:37 +0530

    K-th element of two Arrays.py

--- Dsa_questions/K-th element of two Arrays.py
+++ Dsa_questions/K-th element of two Arrays.py
@@ -1,8 +1,25 @@
 
 class Solution:
-    def kthElement(self, k, arr1, arr2):
-        arr1.extend(arr2)
-        arr1.sort()
-        return arr1[k-1]
-        
-        
+    def select_from_array(self,array, index, k):
+        k-=1
+        if k==0:
+            return array[index],k
+        return None,k
+
+    def kthElement(self, a, b, k):
+        left = 0
+        right = 0
+        result = -1
+        while k > 0:
+            if left < len(a) and (right >= len(b) or a[left] <= b[right]):
+                result, k = self.select_from_array(a,left,k)
+                if result is  None:
+                    left += 1
+            elif right < len(b):
+                result, k = self.select_from_array(b,right,k)
+                if result is None:
+                    right += 1
+                
+        return result
+                
+            

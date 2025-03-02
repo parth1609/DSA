22d1f04a29df79b12aa22d1bcecd28449d928ccc
parth1609 <115941606+parth1609@users.noreply.github.com>
2025-03-02 07:23:46 +0530

    2460. Apply Operations to an Array.py

---
+++ Dsa_questions/2460. Apply Operations to an Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+        idx = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[idx], nums[i]  = nums[i], nums[idx]
+                idx += 1 
+        return nums
